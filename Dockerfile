f40d6f6db8ead70467c18d70099eb144521ba58d
akmot9 <95435335+Akmot9@users.noreply.github.com>
2023-02-14 13:43:37 +0100

    commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM python:3.8-slim-buster
+
+# Install dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential
+RUN apt-get install -y python3-tk
+
+# Install PyInstaller
+RUN pip install pyinstaller
+
+# Copy the entrypoint script and make it executable
+COPY Pyinstaller.sh /Pyinstaller.sh
+RUN chmod +x /Pyinstaller.sh
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Define a volume to store the file
+VOLUME /data
+
+# Set the entrypoint script as the Docker entrypoint
+ENTRYPOINT ["/Pyinstaller.sh"]
+
+CMD ["ls", "-R", "/app/*"]
+# Copy the contents of /app to /data
+CMD ["cp", "-R", "/app/*", "/data/"]
+
+
--- Pyinstaller.sh
+++ Pyinstaller.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo "pyinstaller :"
 
-pyinstaller --noconfirm --onefile --console --add-data "$3:." --add-data "$2:img/" "$1"
-
-#pyinstaller --noconfirm --onefile --console --add-data "/home/akmot/Documents/GitHub/Pyinstaller_i386_Ubunru_12.03/linaudit_py.sh:." --add-data "/home/akmot/Documents/GitHub/Pyinstaller_i386_Ubunru_12.03/img:img/"  "/home/akmot/Documents/GitHub/Pyinstaller_i386_Ubunru_12.03/CyCLOPS.py"
+# Get the input script path from the command line argument
+INPUT_SCRIPT=$1
+echo "script : $1"
+# Use PyInstaller to create a standalone executable
+pyinstaller --noconfirm --onefile --console $INPUT_SCRIPT
+pwd
+ls -la
